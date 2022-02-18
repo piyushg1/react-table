01abbab8bba834ed127e47119ca908bb5a073ebc
Piyush <piyushat25@gmail.com>
2022-02-18 22:39:29 +0530

    working

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,81 @@
 import './App.css';
-
+import React from 'react';
+import { Navbar, Container, InputGroup, FormControl, Table } from 'react-bootstrap'
+import Data from './components/MOCK_DATA.json'
+import Page from './components/Page';
 function App() {
+  const [txt, setTxt] = React.useState('');
+  const itemsPpg = 10;
+  const totNOfPages = Math.ceil(Data.length / itemsPpg)
+  const [pagination, setPagination] = React.useState(
+    {
+      start: 0,
+      end: itemsPpg,
+    }
+  )
+  const onPageChange = (start, end) => {
+    setPagination({ start: start, end: end })
+  }
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Navbar className='mb-2' >
+        <Container>
+          <Navbar.Brand href='http://piyush--portfolio.herokuapp.com/' style={{ color: 'whitesmoke' }}>
+            Piyush
+          </Navbar.Brand>
+          <h1 style={{ color: 'whitesmoke', marginInline: 'auto' }}>React Table</h1>
+        </Container>
+      </Navbar>
+      <Container>
+        <p style={{ color: 'whitesmoke' }}>This app includes a table that retrieves data from the API and maps it to the table, as well as search capability with a pagination mechanism. <br />Click on the upper left corner to see more of my creations.</p>
+        <InputGroup className='mb-4' onChange={(e) => (setTxt(e.target.value))} value={txt}>
+          <InputGroup.Text>First Name</InputGroup.Text>
+          <FormControl as="textarea" aria-label="username" />
+        </InputGroup>
+        <Table striped bordered hover variant='dark'>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              Data.slice(pagination.start, pagination.end)
+                .filter((item) => {
+                  if (txt === '') {
+                    return item
+                  }
+                  if (item.first_name.toLowerCase().includes(txt.toLowerCase())) {
+                    return item
+                  }
+                })
+                .map((item, key) => {
+                  return (
+                    <tr>
+                      <td>
+                        {
+                          key = item.id
+                        }
+                      </td>
+                      <td>
+                        {
+                          item.first_name
+                        }
+                      </td>
+                      <td>
+                        {
+                          item.last_name}
+                      </td>
+                    </tr>
+                  )
+                })
+            }
+          </tbody>
+        </Table>
+        <Page itemsPpg={itemsPpg} onPageChange={onPageChange} totNOfPages={totNOfPages} />
+      </Container>
     </div>
   );
 }
---
+++ src/components/Page.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import Pagination from 'react-bootstrap/Pagination'
+function Page({ itemsPpg, onPageChange, totNOfPages }) {
+    const [counter, setCounter] = React.useState(1);
+    React.useEffect(() => {
+        const value = itemsPpg * counter
+        onPageChange(value - itemsPpg, value)
+    }, [counter])
+    const prevBtn = () => {
+        if (counter === 1) {
+            setCounter(1);
+        }
+        else {
+            setCounter(counter - 1)
+        }
+    }
+    const nextBtn = () => {
+        if (counter === totNOfPages) {
+            setCounter(totNOfPages)
+        }
+        else {
+            setCounter(counter + 1)
+        }
+    }
+    return (
+        <>
+
+            <Pagination size='md' style={{ justifyContent: 'center' }}>
+                <Pagination.Prev onClick={prevBtn} />
+                {
+                    new Array(totNOfPages).fill("").map((item, index) => {
+                        return (
+                            <Pagination.Item active={index + 1 === counter} onClick={() => setCounter(index + 1)}>
+                                {
+                                    index + 1
+                                }
+                            </Pagination.Item>
+                        )
+                    })
+                }
+
+                <Pagination.Next onClick={nextBtn} />
+            </Pagination>
+        </>
+    )
+}
+
+export default Page
